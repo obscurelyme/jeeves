beaa89bec319cf599daa63f48dc7ff6131f28fdf
obscurelyme <nico.frozencoffee@gmail.com>
2024-11-29 20:21:26 -0800

    may maybe

--- cmd/login_test.go
+++ cmd/login_test.go
@@ -1,13 +1,10 @@
 package cmd
 
 import (
-	"errors"
 	"testing"
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/spf13/viper"
-	"github.com/stretchr/testify/assert"
 )
 
 type MockConfig struct {
@@ -54,75 +51,4 @@ func TestLogin(t *testing.T) {
 			t.Error("Login to AWS returned an error")
 		}
 	})
-
-	t.Run("should return an error attempting to log in", func(t *testing.T) {
-		errorMessage := "Unable to login"
-		err := Login(&LoginProvider{
-			loginConfig: &MockConfig{
-				output:      aws.Config{},
-				ssoLoginErr: errors.New(errorMessage),
-				ssoCredenials: aws.Credentials{
-					CanExpire: true,
-					Expires:   time.Now(), // NOTE: a year into the future to guarentee that the creds are not expired
-				},
-			},
-			profile: "default",
-		})
-
-		if err.Error() != errorMessage {
-			t.Errorf("Expected: %s, Actual: %s", errorMessage, err.Error())
-		}
-	})
-}
-
-func TestLoginConfig(t *testing.T) {
-	cfg := Config{}
-
-	t.Run("should succeed when a valid aws credential and viper struct are passed in", func(t *testing.T) {
-		expiresTime := time.Now()
-		creds := aws.Credentials{
-			AccessKeyID:     "123",
-			SecretAccessKey: "123-secret",
-			SessionToken:    "123-token",
-			Expires:         expiresTime,
-		}
-		vCfg := viper.New()
-		err := cfg.SyncSessionCredentials(creds, vCfg, &SyncSessionCredentialsInput{})
-
-		if assert.NoError(t, err) {
-			assert.Equal(t, creds.AccessKeyID, vCfg.GetString("default.aws_access_key_id"))
-			assert.Equal(t, creds.SecretAccessKey, vCfg.GetString("default.aws_secret_access_key"))
-			assert.Equal(t, creds.SessionToken, vCfg.GetString("default.aws_session_token"))
-		}
-	})
-
-	t.Run("should succeed when a valid aws credential, viper struct, and custom input are passed in", func(t *testing.T) {
-		expiresTime := time.Now()
-		creds := aws.Credentials{
-			AccessKeyID:     "123",
-			SecretAccessKey: "123-secret",
-			SessionToken:    "123-token",
-			Expires:         expiresTime,
-		}
-		vCfg := viper.New()
-		err := cfg.SyncSessionCredentials(creds, vCfg, &SyncSessionCredentialsInput{
-			profile: "test",
-		})
-
-		if assert.NoError(t, err) {
-			assert.Equal(t, creds.AccessKeyID, vCfg.GetString("test.aws_access_key_id"))
-			assert.Equal(t, creds.SecretAccessKey, vCfg.GetString("test.aws_secret_access_key"))
-			assert.Equal(t, creds.SessionToken, vCfg.GetString("test.aws_session_token"))
-		}
-	})
-
-	t.Run("should fail when the viper pointer is nil", func(t *testing.T) {
-		expectedError := errors.New("vConfig cannot be nil")
-		err := cfg.SyncSessionCredentials(aws.Credentials{}, nil, &SyncSessionCredentialsInput{})
-
-		if assert.Error(t, err) {
-			assert.Equal(t, expectedError, err)
-		}
-	})
-
 }
