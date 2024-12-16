8eb6acecc1d67c3de299bfbd23ee6887f7d53b97
obscurelyme <nico.frozencoffee@gmail.com>
2024-12-16 15:52:53 -0800

    nova

--- ai/nova/nova.go
+++ ai/nova/nova.go
@@ -1,1 +1,40 @@
 package nova
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/aws/aws-sdk-go-v2/service/bedrockruntime"
+	nova "github.com/obscurelyme/jeeves/ai/nova/types"
+	"github.com/obscurelyme/jeeves/ai/types"
+)
+
+type NovaAi struct {
+	clientId   *bedrockruntime.Client
+	modelId    string
+	tokenCount int32
+}
+
+func (ai *NovaAi) Invoke(prompt string) (string, error) {
+	return "", nil
+}
+
+func (ai *NovaAi) InvokeStream(prompt string, handler types.StreamingOutputHandler) error {
+	return nil
+}
+
+func (ai *NovaAi) processStreamOutput(ctx context.Context,
+	output *bedrockruntime.InvokeModelWithResponseStreamOutput,
+	handler types.StreamingOutputHandler) error {
+	return nil
+}
+
+func (ai *NovaAi) unmarshal(output *bedrockruntime.InvokeModelOutput) (*nova.Body, error) {
+	var response nova.Body
+	err := json.Unmarshal(output.Body, &response)
+	if err != nil {
+		return nil, err
+	}
+
+	return &response, nil
+}
